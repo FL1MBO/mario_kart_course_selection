b1509988b6d9f0f4dbf6c65315f9ce9cc2a4f1ad
BorisDeGraaff <borisdegraaff@gmail.com>
2025-06-14 12:47:37 +0200

    Pick and ban state

--- src/app/test/course-tile.tsx
+++ src/app/test/course-tile.tsx
@@ -2,13 +2,20 @@
 
 import { useState } from "react"
 
-export default function CourseTile({ course_id }: any) {
-  const [select_state, set_selectState] = useState("neutral")
+export default function CourseTile({ course_id, increase_pick_count, currently_picking }: any) {
+  const [select_state, set_select_state] = useState("neutral")
 
   return (
     <div
       className="relative group cursor-pointer"
-      onClick={() => set_selectState("banned")}
+      onClick={() => {
+        increase_pick_count()
+        if (currently_picking === "picking") {
+          set_select_state("picked")
+        } else if (currently_picking === "banning") {
+          set_select_state("banned")
+        }
+      }}
     >
       <img
         src={`/course_images/${course_id}.webp`}
@@ -16,18 +23,65 @@ export default function CourseTile({ course_id }: any) {
         className="w-full h-auto object-cover"
       />
 
-      {/* Red cross overlay */}
-      <div
-        className={`absolute inset-0 ${
-          select_state === "banned"
-            ? "bg-red-600 flex"
-            : "bg-[rgba(220,38,38,0.6)] hidden group-hover:flex"
-        } items-center justify-center`}
-        style={{ pointerEvents: "none" }} // allow clicks to pass through
-      >
-        <div className="w-10 h-1 bg-white rotate-45 absolute" />
-        <div className="w-10 h-1 bg-white -rotate-45 absolute" />
-      </div>
+      {/* Currently banning */}
+      {currently_picking === "banning" && (
+        <div
+          className={`absolute inset-0 bg-[rgba(220,38,38,0.3)] hidden group-hover:flex items-center justify-center`}
+          style={{ pointerEvents: "none" }} // allow clicks to pass through
+        >
+          <div className="w-10 h-1 bg-white rotate-45 absolute" />
+          <div className="w-10 h-1 bg-white -rotate-45 absolute" />
+        </div>
+      )}
+
+      {/* Currently picking */}
+      {currently_picking == "picking" && (
+        <div
+          className={`absolute inset-0 bg-[rgba(53,220,38,0.21)] hidden group-hover:flex items-center justify-center`}
+          style={{ pointerEvents: "none" }} // allow clicks to pass through
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="w-12 h-12 text-white"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+          >
+            <path
+              fillRule="evenodd"
+              d="M20.285 6.709a1 1 0 00-1.414-1.418l-9.192 9.191-4.242-4.24a1 1 0 00-1.415 1.415l5 4.999a1 1 0 001.415 0l9.848-9.847z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </div>
+      )}
+      {/* Already banned */}
+      {select_state === "banned" && (
+        <div
+          className={`absolute inset-0 bg-[rgba(255,12,12,0.51)] flex items-center justify-center`}
+          style={{ pointerEvents: "none" }}>
+          <div className="w-10 h-1 bg-white rotate-45 absolute" />
+          <div className="w-10 h-1 bg-white -rotate-45 absolute" />
+        </div>
+      )}
+      {/* Already picked */}
+      {select_state === "picked" && (
+        <div
+          className={`absolute inset-0 bg-[rgba(81,255,12,0.51)] flex items-center justify-center`}
+          style={{ pointerEvents: "none" }}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="w-12 h-12 text-white"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+          >
+            <path
+              fillRule="evenodd"
+              d="M20.285 6.709a1 1 0 00-1.414-1.418l-9.192 9.191-4.242-4.24a1 1 0 00-1.415 1.415l5 4.999a1 1 0 001.415 0l9.848-9.847z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </div>
+      )}
     </div>
   )
 }
--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,11 +1,34 @@
+"use client"
+
+import { useState } from "react";
 import CourseTile from "./course-tile";
 
 export default function Page() {
+  const [pick_count, set_pick_count] = useState(0)
+  const [currently_picking, set_currently_picking] = useState("banning")
+
+
+  function increase_pick_count() {
+    set_pick_count(pick_count + 1)
+    if (pick_count > 3){
+      set_currently_picking("picking")
+    }
+  }
+
   return (
-    <div className="grid grid-cols-8 grid-rows-4 gap-2">
-      {Array.from({ length: 32 }).map((_, i) => (
-        <CourseTile key={i+1} course_id={i+1}/>
-      ))}
-    </div>
+    <>
+      <div className="grid grid-cols-8 grid-rows-4 gap-2">
+        {Array.from({ length: 32 }).map((_, i) => (
+          <CourseTile key={i + 1}
+          course_id={i + 1}
+          increase_pick_count={increase_pick_count}
+          pick_count={pick_count}
+          currently_picking={currently_picking}
+          />
+        ))}
+      </div>
+      <div>{pick_count}</div>
+      <div>{currently_picking}</div>
+    </>
   );
 }
